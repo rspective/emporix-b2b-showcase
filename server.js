30845e523404f8e49d96cca1e4026f648256d990
p-zielinski <82354717+p-zielinski@users.noreply.github.com>
2023-06-15 17:01:38 +0200

    cli via get requests

--- server.js
+++ server.js
@@ -3,6 +3,18 @@ const express = require('express')
 const app = express()
 const dotenv = require('dotenv')
 const path = require('path')
+const {
+  migrateCampaignsToContentful,
+} = require('./CLI/migrate-campaigns-to-contentfull')
+const {
+  migratePromotionTiersToContentful,
+} = require('./CLI/migrate-promotion-tiers-to-contentfull')
+const {
+  migrateStandaloneVouchersToContentful,
+} = require('./CLI/migrate-standalone-vouchers-to-contentfull')
+const {
+  migrateProductsToVoucherify,
+} = require('./CLI/migrate-products-to-voucherify')
 dotenv.config()
 // const checkExpiredEmailValidation = require("./modules/checkExpiredEmailValidation");
 app.use(express.urlencoded({ extended: false }))
@@ -23,6 +35,28 @@ app.use(express.static('build', options))
 
 app.get(`/${process.env.SECRET_PATH}*`, (req, res) => {
   res.sendFile(path.resolve(__dirname, 'build', 'index.html'))
+})
+
+app.get('/migrate-all-promotions', async (req, res) => {
+  res.json({ success: true, operation: 'started' })
+  try {
+    await migrateCampaignsToContentful()
+    await migratePromotionTiersToContentful()
+    await migrateStandaloneVouchersToContentful()
+  } catch (e) {
+    console.log(e)
+  }
+  return
+})
+
+app.get('/migrate-all-products', async (req, res) => {
+  res.json({ success: true, operation: 'started' })
+  try {
+    await migrateProductsToVoucherify()
+  } catch (e) {
+    console.log(e)
+  }
+  return
 })
 
 const PORT = process.env.PORT || 5555

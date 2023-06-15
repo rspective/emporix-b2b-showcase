f35eea26fc584593f69efa339796872612eeb65d
p-zielinski <82354717+p-zielinski@users.noreply.github.com>
2023-06-15 16:48:06 +0200

    in progress

--- server.js
+++ server.js
@@ -11,24 +11,21 @@ app.use((req, res, next) => {
   express.json()(req, res, next)
 })
 
-if (process.env.NODE_ENV === 'production') {
-  const options = {
-    dotfiles: 'ignore',
-    etag: false,
-    extensions: ['htm', 'html'],
-    index: false,
-    maxAge: '1d',
-    redirect: false,
-  }
-  app.use(express.static('admin/build', options))
-  app.use(express.static('client/build', options))
-
-  app.get(`/${process.env.SECRET_PATH}*`, (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'build', 'index.html'))
-  })
+const options = {
+  dotfiles: 'ignore',
+  etag: false,
+  extensions: ['htm', 'html'],
+  index: false,
+  maxAge: '1d',
+  redirect: false,
 }
+app.use(express.static('build', options))
 
-const PORT = process.env.PORT || 5000
+app.get(`/${process.env.SECRET_PATH}*`, (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'build', 'index.html'))
+})
+
+const PORT = process.env.PORT || 5555
 
 const main = (async () => {
   app.listen(PORT, () => console.log(`Server started on port ${PORT}!`))

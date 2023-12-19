a55245cb629b0e5f71b14ae06ef222ea15544ca9
p-zielinski <82354717+p-zielinski@users.noreply.github.com>
2023-12-19 12:07:38 +0100

    Revert "Revert "Update groupAndSortQualifications.js""
    This reverts commit c2972c7a3433e86823291d820a69b4b9e1c7bf4e.

--- src/utils/groupAndSortQualifications.js
+++ src/utils/groupAndSortQualifications.js
@@ -1,4 +1,4 @@
-import { compact } from 'lodash'
+import { compact, sortBy } from 'lodash'
 
 export const groupAndSortQualifications = (qualifications) => {
   const loyaltyCards = qualifications
@@ -11,24 +11,48 @@ export const groupAndSortQualifications = (qualifications) => {
         )
       ),
     }))
+  const groups = {}
   let result = loyaltyCards
   qualifications
     .filter((qualification) => qualification.type !== 'LOYALTY_CARD')
     .forEach((qualification) => {
-      const indexOfLoyaltyCard = result.findIndex((resultQualification) =>
+      const loyaltyCard = result.find((resultQualification) =>
         resultQualification.rewardCampaignsIds?.includes?.(
           qualification.campaign_id
         )
       )
-      if (typeof indexOfLoyaltyCard !== 'number' || indexOfLoyaltyCard < 0) {
-        result.push(qualification)
+      if (!loyaltyCard) {
+        return result.push(qualification)
+      }
+      if (groups[loyaltyCard.id]) {
+        groups[loyaltyCard.id] = [...groups[loyaltyCard.id], qualification]
       } else {
-        result = [
-          ...result.slice(0, indexOfLoyaltyCard + 1),
-          qualification,
-          ...result.slice(indexOfLoyaltyCard + 1),
-        ]
+        groups[loyaltyCard.id] = [qualification]
       }
     })
+  Object.entries(groups).forEach((qualificationIdAndQualifications) => {
+    const [id, qualifications] = qualificationIdAndQualifications
+    const indexOfLoyaltyCard = result.findIndex(
+      (resultQualification) => resultQualification.id === id
+    )
+    if (typeof indexOfLoyaltyCard !== 'number' || indexOfLoyaltyCard < 0) {
+      return console.log('indexOfLoyaltyCard not found :/')
+    }
+    result = [
+      ...result.slice(0, indexOfLoyaltyCard + 1),
+      ...qualifications.sort((q1, q2) => {
+        try {
+          return (
+            new Date(q1.created_at).getTime() <
+            new Date(q2.created_at).getTime()
+          )
+        } catch (e) {
+          console.log(e)
+          return false
+        }
+      }),
+      ...result.slice(indexOfLoyaltyCard + 1),
+    ]
+  })
   return result
 }
